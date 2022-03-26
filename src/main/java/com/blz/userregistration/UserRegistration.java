6b3771454bcea4e905162714805317363f44d14b
TejShind <tej.gholap25@gmail.com>
2022-03-26 11:46:59 +0530

    [Tejaswini]ADDED:Feature for  Create JUnit test to validate multiple entry as per given Eamil address

--- src/main/java/com/blz/userregistration/UserRegistration.java
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public final String firstName = "^[A-Z]{1}[a-z]{2,}$";//Name starts with Cap and has minimum 3 characters
     public final String lastName = ("^[A-Z]{1}[a-z]{3,}$");//Name starts with Cap and has minimum 3 characters
-    public final String email = "^[a-z]{3,}(.[a-z]{3,})*@[a-z]{2,}.[a-z]{2,3}([.+_-][a-z]{2})*$";//e.g abc.xyz@blz.co.in
     public final String mobileNumber = "^[0-9]{2}\s?[0-9]{10}$";//e.g 91 991981980
     public final String passWord = "(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]*[@#$%_!^&*][0-9a-zA-Z]*";
     public final String sampleEmail = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
